cadcd120055e733e077b11f539394eabc2e8dd5b
Jed556 <jguiriba11@gmail.com>
2023-06-27 03:50:02 +0800

    fix: Wrong position after returning from collision

--- src/engine/player/player.cpp
+++ src/engine/player/player.cpp
@@ -9,17 +9,27 @@ void Player::update() {
     if (IsKeyDown(KEY_W)) position.y -= math::normSpeed(speed);
 }
 
-
 bool Player::collide(Vector2 start, Vector2 end) {
     bool collided = false;
-    if (position.x < start.x || position.x + size.x > start.x + end.x) {
-        position.x = prevPosition.x;
+    Vector2 newPosition = position;
+
+    if (position.x < start.x) {
+        newPosition.x = start.x;
+        collided = true;
+    } else if (position.x + size.x > start.x + end.x) {
+        newPosition.x = start.x + end.x - size.x;
         collided = true;
     }
-    if (position.y < start.y || position.y + size.y > start.y + end.y) {
-        position.y = prevPosition.y;
+
+    if (position.y < start.y) {
+        newPosition.y = start.y;
+        collided = true;
+    } else if (position.y + size.y > start.y + end.y) {
+        newPosition.y = start.y + end.y - size.y;
         collided = true;
     }
+
+    if (collided) position = newPosition;
     return collided;
 }
 

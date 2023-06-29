cebf5087eaf2a436034c296a81a499987925b4c2
Jed556 <jguiriba11@gmail.com>
2023-06-29 16:48:27 +0800

    feat: Add leading zeroes adder function

--- src/engine/math/math.cpp
+++ src/engine/math/math.cpp
@@ -2,6 +2,18 @@
 
 namespace math {
 float normSpeed(float speed) {
+    // DO NOT CHANGE THIS ALL SYSTEM SPEEDS RELY ON THIS
     return speed * 10.0f * GetFrameTime();
+}
+
+std::string addLeadingZeroes(double num, int targetLength, bool zeroesOnly) {
+    std::ostringstream ss;
+    ss << num;
+    std::string str = ss.str();
+    int len = str.length();
+    std::string zeroes = "";
+    if (len < targetLength) zeroes = std::string(targetLength - len, '0');
+
+    return zeroes + (zeroesOnly ? "" : str);
 }
 }  // namespace math

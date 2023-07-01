51c32c66946b32165e9de81a68787e764ce47e3d
Jed556 <jguiriba11@gmail.com>
2023-07-02 01:23:42 +0800

    chore: Add parser for dev

---
+++ src/game/parse.cpp
@@ -0,0 +1,8 @@
+#include "../engine/data/data.h"
+
+// void data::parse(GameData data) {
+//     player = data.player;
+//     player2 = data.player2;
+//     player3 = data.player3;
+//     control = data.control;
+// }

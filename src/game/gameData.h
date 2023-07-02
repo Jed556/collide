4221b3cb5947bad01074123cf73cd6a45dd1df1e
Jed556 <jguiriba11@gmail.com>
2023-07-02 14:44:59 +0800

    feat: Add user defined parsing for load and save

--- src/game/gameData.h
+++ src/game/gameData.h
@@ -5,11 +5,29 @@
 
 #include "../engine/engine.h"
 
+// Change the GameData and GameDataRef structs according to your game data
+
+/**
+ * @brief Game data to be saved
+ *
+ */
 struct GameData {
     Entity player;
     Entity player2;
     Entity player3;
     int control;
+};
+
+/**
+ * @brief References to game data from your game
+ * @brief where the data will be loaded
+ *
+ */
+struct GameDataRef {
+    Entity& player;
+    Entity& player2;
+    Entity& player3;
+    int& control;
 };
 
 #endif  // GAMEDATA_H
--- src/game/parse.cpp
+++ src/game/parse.cpp
@@ -1,8 +1,10 @@
 #include "../engine/data/data.h"
 
-// void data::parse(GameData data) {
-//     player = data.player;
-//     player2 = data.player2;
-//     player3 = data.player3;
-//     control = data.control;
-// }
+void data::parse(GameData* loadedData, GameDataRef& gameData) {
+
+    // Change according to your game data
+    gameData.control = loadedData->control;
+    gameData.player = loadedData->player;
+    gameData.player2 = loadedData->player2;
+    gameData.player3 = loadedData->player3;
+}

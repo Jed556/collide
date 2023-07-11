205f8bbbd5fc4d23ec388d15d2078b1bd460630f
Jed556 <jguiriba11@gmail.com>
2023-07-11 10:46:49 +0800

    refactor: Add necessary window flags

--- src/engine/display/window.cpp
+++ src/engine/display/window.cpp
@@ -4,6 +4,7 @@ namespace display {
 void init(Vector2 dimension, const char* title, const char* iconPath) {
     Image icon = LoadImage(iconPath);
     InitWindow((int)dimension.x, (int)dimension.y, title);
+    SetWindowState(FLAG_WINDOW_HIGHDPI | FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_UNDECORATED);
     SetWindowIcon(icon);
 }
 
